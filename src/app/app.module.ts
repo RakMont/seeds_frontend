650c0fdfd817e872ba4b6789e070be755b09ba84
RakMont <nabogaria@gmail.com>
2023-04-02 20:56:26 -0400

    navbar & translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,112 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+//import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic' ;
+import { CKEditorModule } from 'ng2-ckeditor';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+/* Material */
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatMenuModule } from '@angular/material/menu';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AngularEditorModule } from '@kolkov/angular-editor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NavbarComponent } from './core/navbar/navbar.component';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatTreeModule,
+    MatToolbarModule,
+    //  MatCarouselModule.forRoot(),
+    CdkAccordionModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    CdkStepperModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatBottomSheetModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatTooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularEditorModule,
+    CKEditorModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,64 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'sml-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  @Output() emitLen: EventEmitter<string> = new EventEmitter();
+  @Input() inputSideNav: MatSidenav;
+  showFiller = false;
+  events: string[] = [];
+  opened: boolean;
+  panelOpenState = false;
+  snav: MatSidenav;
+
+  loggedIn: boolean = false;
+
+  currentUser;
+  loadingUser = true;
+  constructor(
+    private router: Router,
+    //private tokenService: TokenService,
+    //private oauthService: OauthService,
+  ) { }
+
+
+  display = false;
+  // tslint:disable-next-line:typedef
+  onPress() {
+    this.display = !this.display;
+  }
+
+  logOut(): void {
+    /*this.oauthService.logout();
+    window.location.reload();*/
+  }
+
+  ngOnInit(): void {
+    //this.getUser();
+  }
+
+  /*getUser(): void{
+    this.oauthService.getCurrentUser().subscribe((usr)=>{
+      this.oauthService.setUser(usr);
+      this.currentUser = usr;
+      this.loadingUser = false;
+      this.loggedIn = true;
+    }, (error => {
+      this.inputSideNav.close();
+      this.currentUser = null;
+      this.loadingUser = false;
+      this.loggedIn = false;
+    }));
+  }
+  refreshToken(): void {
+  }
+*/
+  changeLen(len: string){
+    this.emitLen.emit(len);
+  }
+}
