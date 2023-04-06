1ac6a9f4f503520ca67cd57ab9ec57e15cfef2da
RakMont <nabogaria@gmail.com>
2023-04-06 16:44:32 -0400

    send form data message component

---
+++ src/app/feature/seeds-manage/modal-view-seed/modal-view-seed.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalViewSeedComponent } from './modal-view-seed.component';
+
+describe('ModalViewSeedComponent', () => {
+  let component: ModalViewSeedComponent;
+  let fixture: ComponentFixture<ModalViewSeedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModalViewSeedComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ModalViewSeedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/feature/seeds-manage/modal-view-seed/modal-view-seed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-view-seed',
+  templateUrl: './modal-view-seed.component.html',
+  styleUrls: ['./modal-view-seed.component.scss']
+})
+export class ModalViewSeedComponent {
+
+}
